b2518a414bb79eae976ad5f463293f2d96be06e1
sharunashwanth <137257609+sharunashwanth@users.noreply.github.com>
2026-01-02 13:54:54 +0530

    model typo

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 
 class Settings:
     OPENROUTER_APIKEY: str = os.getenv("OPENROUTER_APIKEY")
-    MOODEL_NAME: str = os.getenv("MODEL_NAME")
+    MODEL_NAME: str = os.getenv("MODEL_NAME")
     OPENWEATHERMAP_APIKEY: str = os.getenv("OPENWEATHERMAP_APIKEY")
     SERPAPI_APIKEY: str = os.getenv("SERPAPI_APIKEY")
     DATABASE_URL: str = os.getenv("DATABASE_URL")
